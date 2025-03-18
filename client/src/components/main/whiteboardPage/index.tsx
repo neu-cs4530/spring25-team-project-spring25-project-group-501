3e6ccc6b78de8a3717de163815ea99adb7044cd5
bancheta21 <ancheta.b@northeastern.edu>
2025-03-18 15:56:17 -0400

    quick changes

--- client/src/components/main/whiteboardPage/index.tsx
+++ client/src/components/main/whiteboardPage/index.tsx
@@ -13,6 +13,7 @@ const WhiteboardPage = () => {
   const [title, setTitle] = useState<string>('');
   const [accessType, setAccessType] = useState<string>('read-only');
   const { username } = useParams<{ username: string }>();
+  const [isCreating, setIsCreating] = useState<boolean>(false);
 
   useEffect(() => {
     if (context) {
@@ -72,7 +73,10 @@ const WhiteboardPage = () => {
     // <button className='btn-create-whiteboard' onClick={handleCreateWhiteboard}>Create Whiteboard</button>
     <div className='whiteboard-container'>
       <div className='horizontal-flex'>
-        <button className='btn-create-whiteboard' onClick={handleCreateWhiteboard}>Create Whiteboard</button>
+        {!isCreating && <button className='btn-create-whiteboard' onClick={() => setIsCreating(!isCreating)}>Start Creating Whiteboard</button>}
+        {isCreating && <button className='btn-create-whiteboard' onClick={handleCreateWhiteboard}>Create Whiteboard</button>}
+        {isCreating &&
+        <input type='text' value={title} onChange={e => setTitle(e.target.value)}></input>}
         <div className='horizontal-flex'>
           <p>Size: </p>
           <input
